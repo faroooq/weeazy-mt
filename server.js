746f302a9b9e3444c90150da8aaaa6f2d02ccf30
faroooq <faroooq.in@gmail.com>
2022-04-15 22:47:53 +0530

    chagne

--- server.js
+++ server.js
@@ -8,22 +8,28 @@ const multer = require("multer");
 const compression = require('compression');
 const cors = require("cors");
 
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://www.weeazy.org");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+let corsOptions = {
+  // origin: "*"
+  origin: ['https://www.weeazy.org', process.env.WEEAZY_UI_REMOTE_URL]
+};
+app.use(cors(corsOptions));
+
 //Routes import
 const authRoutes = require("./routes/auth");
 const ticketRoutes = require("./routes/ticket");
 const userRoutes = require("./routes/user");
 const projectRoutes = require("./routes/project");
 const teamRoutes = require("./routes/team");
-
-const app = express();
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
 
 const dbURI = process.env.dbURI || "mongodb://localhost:27017/bug_tracker";
 
@@ -34,11 +40,6 @@ mongoose
     console.log(`DB Connection Error: ${err.message}`);
   });
 
-// let corsOptions = {
-//   // origin: "*"
-//   origin: ['https://www.weeazy.org', process.env.WEEAZY_UI_REMOTE_URL]
-// };
-// app.use(cors(corsOptions));
 app.use(compression());
 
 // parse requests of content-type - application/json
