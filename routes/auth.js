2f07b72d0a011597ab0449e4e0cfac62a1e0069e
faroooq <faroooq.in@gmail.com>
2022-07-29 23:20:04 +0530

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+
 router.post("/signup", (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then((hash) => {
     const user = new User({
